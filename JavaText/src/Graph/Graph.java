b2b352f7d3c8c2ca09d4e8a9b8d99d4f5bc78302
zlb-2002 <1335654255@qq.com>
2021-12-08 20:07:14 +0800

    commit 2021 12 08

--- JavaText/src/Graph/Graph.java
+++ JavaText/src/Graph/Graph.java
@@ -8,14 +8,14 @@ import java.util.LinkedList;
  * 图的邻接矩阵的表示方法
  */
 
-public class Graph1 {
+public class Graph {
 
     private ArrayList<String> vertexList;        //存储顶点集合
     private int [][] edges;                     //存储图对应的邻接矩阵
     private int numOfEdges;                     //表示边的数目
     private boolean[] isVisited;
 
-    public Graph1(int n) {
+    public Graph(int n) {
         edges = new int[n][n];
         vertexList = new ArrayList<>(n);
         numOfEdges = 0;
@@ -34,7 +34,6 @@ public class Graph1 {
      */
     public void insertEdge (int v1, int v2, int weight) {
         edges[v1][v2] = weight;
-        edges[v2][v1] = weight;
         numOfEdges++;
     }
 
@@ -58,6 +57,7 @@ public class Graph1 {
         return edges[v1][v2];
     }
 
+    //打印邻接矩阵
     public void showGraph () {
         for (int[] link : edges) {
             System.out.println(Arrays.toString(link));
@@ -88,10 +88,10 @@ public class Graph1 {
         return -1;
     }
 
-    //深搜
+    //深度优先搜索
     private void dfs(boolean[] isVisited, int i) {
         //首先访问该节点
-        System.out.print(getValueByIndex(i) + "->");
+        System.out.print(getValueByIndex(i) + " ");
         //将此节点设置为已访问
         isVisited[i] = true;
         //查找结点i的第一个邻接结点
@@ -104,7 +104,9 @@ public class Graph1 {
         }
     }
 
+    //深度优先搜索
     public void dfs() {
+        System.out.println("深度优先搜索：");
         isVisited = new boolean[getNumOfVertex()];
         for (int i = 0; i < getNumOfVertex(); i++) {
             if (!isVisited[i]) {
@@ -113,11 +115,13 @@ public class Graph1 {
         }
     }
 
+
+    //广度优先搜索
     private void bfs(boolean[] isVisited, int i) {
         int u;
         int w;
         LinkedList<Integer> queue = new LinkedList<>();
-        System.out.print(getValueByIndex(i)+ "->");
+        System.out.print(getValueByIndex(i)+ " ");
         isVisited[i] = true;
         queue.addLast(i);
         while (!queue.isEmpty()) {
@@ -125,7 +129,7 @@ public class Graph1 {
             w = getFistNeighbor(u);
             while (w != -1) {
                 if (!isVisited[w]) {
-                    System.out.print(getValueByIndex(w) + "->");
+                    System.out.print(getValueByIndex(w) + " ");
                     isVisited[w] = true;
                     queue.addLast(w);
                 }
@@ -135,8 +139,10 @@ public class Graph1 {
         }
     }
 
+    //广度优先搜索
     public void bfs () {
         isVisited = new boolean[getNumOfVertex()];
+        System.out.println("广度优先搜索：");
         for (int i = 0; i < getNumOfVertex(); i++) {
             if (!isVisited[i]) {
                 bfs(isVisited, i);
@@ -144,46 +150,25 @@ public class Graph1 {
         }
     }
 
-    /*public static void main(String[] args) {
-        *//*int n = 5;
-        String vertexValue[] = {"A", "B", "C", "D", "E"};
-        Graph1 graph1 = new Graph1(n);
-        for (String value : vertexValue) {
-            graph1.insertVertex(value);
+    //计算出度
+    public int outDegree (String vertex) {
+        int count = 0;
+        for (int i = 0;i < getNumOfVertex();i++) {
+            if (edges[vertexList.indexOf(vertex)][i] != 0) {
+                count++;
+            }
         }
-        graph1.insertEdge(0, 1, 1);
-        graph1.insertEdge(0, 2, 1);
-        graph1.insertEdge(1, 2, 1);
-        graph1.insertEdge(1, 3, 1);
-        graph1.insertEdge(1, 4, 1);
-        graph1.showGraph();
-        System.out.println("深度优先搜索");
-        graph1.dfs();
-        System.out.println();
-        System.out.println("广度优先搜索");
-        graph1.bfs();
+        return count;
+    }
 
-        *//*
-        Graph1 graph11 = new Graph1(8);
-        String[] vertexes = {"1","2","3","4","5","6","7","8"};
-        for (String vertex : vertexes) {
-            graph11.insertVertex(vertex);
+    //计算入度
+    public int inDegree (String vertex) {
+        int count = 0;
+        for (int i = 0;i < getNumOfVertex();i++) {
+            if (edges[i][vertexList.indexOf(vertex)] != 0) {
+                count++;
+            }
         }
-        graph11.insertEdge(0,1,1);
-        graph11.insertEdge(0,2,1);
-        graph11.insertEdge(1,3,1);
-        graph11.insertEdge(0,4,1);
-        graph11.insertEdge(3,7,1);
-        graph11.insertEdge(4,7,1);
-        graph11.insertEdge(2,5,1);
-        graph11.insertEdge(2,6,1);
-        graph11.insertEdge(5,6,1);
-        graph11.showGraph();
-        System.out.println("深搜");
-        graph11.dfs();
-        System.out.println();
-        System.out.println("广搜");
-        graph11.bfs();
-     }*/
-
+        return count;
+    }
 }
